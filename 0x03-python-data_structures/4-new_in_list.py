cfb6fc68a0ea556407f32052a5264e1ef6c419ad
Sohail Charef <charefsohayl@gmail.com>
2023-10-07 03:47:51 -0700

    Replace in a copy list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copylist = []
     if idx < 0 or idx >= len(my_list):
         return my_list
-    for i in range(0, len(my_list)):
-        copylist.append(my_list[i])
-        copylist = replace_in_list(copylist, idx, element)
-        return copylist
-def replace_in_list(my_list, idx, element):
-    my_list[idx] = element
-    return my_list
+
+    copylist = my_list.copy()
+    copylist[idx] = element
+    return copylist
