ada7fc32d9035bfe94971e23f2903c0564a042c9
Sohail Charef <charefsohayl@gmail.com>
2023-10-11 08:01:58 -0700

    replce

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -8,3 +8,4 @@ def search_replace(my_list, search, replace):
         else:
             new_list.append(element)
     return new_list
+"""return list(map(lambda e: replace if e == search else e, my_list))"""
