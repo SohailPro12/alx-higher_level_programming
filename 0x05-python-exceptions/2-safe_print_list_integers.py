2d2b2ef1fd622ccd1ff3aef77341973a26e9698b
Sohail Charef <charefsohayl@gmail.com>
2023-10-23 11:14:39 -0700

    print just int and count

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,5 @@
-afe_print_list_integers(my_list=[], x=0):
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
     c = 0
     i = 0
     while i < x:
@@ -8,5 +9,5 @@ afe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass
         i += 1
-        print()
-        return c
+    print()
+    return c
