c1997152a035f90d7adcc5f7a4b44abd219c4e48
Sohail Charef <charefsohayl@gmail.com>
2023-10-25 09:40:20 -0700

    print

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -63,9 +63,9 @@ class Square:
           """Define the print() representation of a Square."""
           if self.__size != 0:
               [print("") for i in range(0, self.__position[1])]
-              for i in range(0, self.__size):
-                  [print(" ", end="") for j in range(0, self.__position[0])]
-                  [print("#", end="") for k in range(0, self.__size)]
-                  if i != self.__size - 1:
-                      print("")
-              return ("")
+          for i in range(0, self.__size):
+              [print(" ", end="") for j in range(0, self.__position[0])]
+              [print("#", end="") for k in range(0, self.__size)]
+              if i != self.__size - 1:
+                  print("")
+          return ("")
