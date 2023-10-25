145ce9584ccbd735f8c60fb96e1002e6c63fff8c
Sohail Charef <charefsohayl@gmail.com>
2023-10-25 06:59:23 -0700

    class with one attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines class square."""
+    def __init__(self, size):
+    """Constructor.
+    Args:
+    size: length of a side of the square.
+    """
+    self._size = size
