34fdb1a845791ac131ae5ac624e8fb0185aa6653
Sohail Charef <charefsohayl@gmail.com>
2023-10-30 04:34:22 -0700

    defines the widht and the height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,7 +8,6 @@ class Rectangle:
     """ Rectangle class defines the width and 
     the height.
     """
-
     def __init__(self, width=0, height=0):
         """initilizes the height and the width"""
         self.__height = height
@@ -22,7 +21,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set the width"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -36,7 +35,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set the height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
