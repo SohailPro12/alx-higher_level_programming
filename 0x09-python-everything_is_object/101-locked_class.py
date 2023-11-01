916f914d5d18ead801ba86d7272fd0dc9bf670d4
Sohail Charef <charefsohayl@gmail.com>
2023-11-01 05:10:25 -0700

    you can't add an attribut

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Module that contain one class LockedClass
+"""
+
+
 class LockedClass:
+    """ Locked Class 
+    you can not add any other attributs
+    """
+
     __slots__ = ['first_name']
