20706f78555fa7ba74b0351c05750035079fe799
Sohail Charef <charefsohayl@gmail.com>
2023-11-06 03:34:23 -0800

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module for MyList class
+"""
+
+
+class MyList(list):
+    """
+    MyList class for a list.
+    """
+
+    def print_sorted(self):
+        """
+        Method for printing sorted list.
+        """
+        print(sorted(self))
