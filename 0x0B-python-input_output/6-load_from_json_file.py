abf793bea9328db6b256b71dca4cc057a1e89a30
Sohail Charef <charefsohayl@gmail.com>
2023-11-07 12:22:49 -0800

    creates an Object from jsonf

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Module with load_form_json_file method
+"""
+import json
+
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a “JSON file”"""
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
