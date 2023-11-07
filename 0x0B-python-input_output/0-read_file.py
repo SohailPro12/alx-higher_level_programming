c38518df886f9fea45d73d156c3822acdaaedf7a
Sohail Charef <charefsohayl@gmail.com>
2023-11-07 06:49:37 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ Module with read_file function.
 def read_file(filename=""):
     """read the file"""
     with open(filename, 'r', encoding='UTF8') as file:
-        print(file.read())
+        print(file.read(), end="")
