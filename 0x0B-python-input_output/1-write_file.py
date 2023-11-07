9d683541e55fea23da08de19a20379b0e13a232f
Sohail Charef <charefsohayl@gmail.com>
2023-11-07 06:58:04 -0800

    read

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ Module with write_file function.
 
 def write_file(filename="", text=""):
     """write in the file"""
-    with open('example.txt', 'w',  encoding='UTF8') as file:
+    with open(filename, 'w',  encoding='UTF8') as file:
         return file.write(text)
