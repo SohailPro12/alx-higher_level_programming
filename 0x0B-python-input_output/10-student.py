6819318eca322947cf8a879ebe2e0ba0838fd226
Sohail Charef <charefsohayl@gmail.com>
2023-11-08 03:37:11 -0800

    attrs

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Module of Student class.
+"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """initiliaze args"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the attrs that were given"""
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        mydict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                mydict[key] = value
+        return mydict
