c72e67bbd4eab6827b01b9b4f0cd9623cda53124
Sohail Charef <charefsohayl@gmail.com>
2023-11-10 07:54:26 -0800

    Update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,27 +1,21 @@
-#!/usr/bin/python3
-
+# models/base.py
 """
-Module of Base class.
+This module defines a Base class that serves as the base for other classes in the project.
 """
 
+import json
 
 class Base:
     """
-    It keeps track of the number of objects created
-    and assigns a unique id to each object.
+    Base class for the project. It includes methods for JSON string conversion and file saving.
     """
-
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        Constructor for the Base class.
+        Initialize a new Base instance.
 
-        Parameters:
-        id (int): The id to assign to the object.
-
-        Returns:
-        None
+        :param id: id of the instance
         """
         if id is not None:
             self.id = id
@@ -29,9 +23,29 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        """json string representation"""
+        """
+        Convert a list of dictionaries to a JSON string.
+
+        :param list_dictionaries: list of dictionaries to convert
+        :return: JSON string representation of the list of dictionaries
+        """
         if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Save a list of objects to a file in JSON string format.
+
+        :param list_objs: list of objects to save
+        """
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        list_dicts = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, mode="w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_dicts))
