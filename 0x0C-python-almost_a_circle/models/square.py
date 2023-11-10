2e82d5365c744f4e5b52517be1e146ccf7af65e0
Sohail Charef <charefsohayl@gmail.com>
2023-11-10 06:14:23 -0800

    Square inherts from rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,3 +16,14 @@ class Square(Rectangle):
     def __str__(self):
         """str implementation"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Width value of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square"""
+        self.width = value
+        self.height = value
