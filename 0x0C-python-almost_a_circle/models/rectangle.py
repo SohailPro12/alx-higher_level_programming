89130fa3c1d06d44d4862f91af2f53ad45cf9702
Sohail Charef <charefsohayl@gmail.com>
2023-11-10 03:24:13 -0800

    Update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 """
 This module defines a Rectangle class that inherits from Base.
 """
-from models.base import Base
 
+from models.base import Base
 
 class Rectangle(Base):
     """
@@ -111,109 +110,14 @@ class Rectangle(Base):
         This module defines a Rectangle class that inherits from Base.
         """
 
-        from models.base import Base
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
 
-        class Rectangle(Base):
-            """
-            Rectangle class that inherits from Base.
-            """
+    def display(self):
+        """Display the rectangle instance"""
+        for i in range(self.height):
+            for j in range(self.__width):
+                print("#", end="")
+            print("")
 
-            def __init__(self, width, height, x=0, y=0, id=None):
-                """
-                Initialize a new Rectangle.
-
-                :param width: width of the rectangle
-                :param height: height of the rectangle
-                :param x: x coordinate of the rectangle
-                :param y: y coordinate of the rectangle
-                :param id: id of the rectangle
-                """
-                super().__init__(id)
-                self.width = width
-                self.height = height
-                self.x = x
-                self.y = y
-
-            @property
-            def width(self):
-                """
-                Get the width of the rectangle.
-                """
-                return self.__width
-
-            @width.setter
-            def width(self, value):
-                """
-                Set the width of the rectangle.
-
-                :param value: new width of the rectangle
-                """
-                if not isinstance(value, int):
-                    raise TypeError("width must be an integer")
-                if value <= 0:
-                    raise ValueError("width must be > 0")
-                self.__width = value
-
-            @property
-            def height(self):
-                """
-                Get the height of the rectangle.
-                """
-                return self.__height
-
-            @height.setter
-            def height(self, value):
-                """
-                Set the height of the rectangle.
-
-                :param value: new height of the rectangle
-                """
-                if not isinstance(value, int):
-                    raise TypeError("height must be an integer")
-                if value <= 0:
-                    raise ValueError("height must be > 0")
-                self.__height = value
-
-            @property
-            def x(self):
-                """
-                Get the x coordinate of the rectangle.
-                """
-                return self.__x
-
-            @x.setter
-            def x(self, value):
-                """
-                Set the x coordinate of the rectangle.
-
-                :param value: new x coordinate of the rectangle
-                """
-                if not isinstance(value, int):
-                    raise TypeError("x must be an integer")
-                if value < 0:
-                    raise ValueError("x must be >= 0")
-                self.__x = value
-
-            @property
-            def y(self):
-                """
-                Get the y coordinate of the rectangle.
-                """
-                return self.__y
-
-            @y.setter
-            def y(self, value):
-                """
-                Set the y coordinate of the rectangle.
-
-                :param value: new y coordinate of the rectangle
-                """
-                if not isinstance(value, int):
-                    raise TypeError("y must be an integer")
-                if value < 0:
-                    raise ValueError("y must be >= 0")
-                self.__y = value
-
-            def area(self):
-                """Returns the area of the rectangle"""
-                return self.__width * self.__height
