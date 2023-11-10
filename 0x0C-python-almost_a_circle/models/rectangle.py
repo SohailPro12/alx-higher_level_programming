187b4c3087e7d0d6a64cb99310427d9e5e75a6d6
Sohail Charef <charefsohayl@gmail.com>
2023-11-10 03:10:29 -0800

    Update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 """
 This module defines a Rectangle class that inherits from Base.
 """
-
 from models.base import Base
+
 
 class Rectangle(Base):
     """
@@ -40,8 +41,12 @@ class Rectangle(Base):
 
         :param value: new width of the rectangle
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
-
+    
     @property
     def height(self):
         """
@@ -56,8 +61,12 @@ class Rectangle(Base):
 
         :param value: new height of the rectangle
         """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
-
+    
     @property
     def x(self):
         """
@@ -72,8 +81,12 @@ class Rectangle(Base):
 
         :param value: new x coordinate of the rectangle
         """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
-
+    
     @property
     def y(self):
         """
@@ -88,4 +101,114 @@ class Rectangle(Base):
 
         :param value: new y coordinate of the rectangle
         """
-        self.__y = value
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value#!/usr/bin/python3
+        """
+        This module defines a Rectangle class that inherits from Base.
+        """
+        
+        from models.base import Base
+        
+        class Rectangle(Base):
+            """
+            Rectangle class that inherits from Base.
+            """
+        
+            def __init__(self, width, height, x=0, y=0, id=None):
+                """
+                Initialize a new Rectangle.
+        
+                :param width: width of the rectangle
+                :param height: height of the rectangle
+                :param x: x coordinate of the rectangle
+                :param y: y coordinate of the rectangle
+                :param id: id of the rectangle
+                """
+                super().__init__(id)
+                self.width = width
+                self.height = height
+                self.x = x
+                self.y = y
+        
+            @property
+            def width(self):
+                """
+                Get the width of the rectangle.
+                """
+                return self.__width
+        
+            @width.setter
+            def width(self, value):
+                """
+                Set the width of the rectangle.
+        
+                :param value: new width of the rectangle
+                """
+                if not isinstance(value, int):
+                    raise TypeError("width must be an integer")
+                if value <= 0:
+                    raise ValueError("width must be > 0")
+                self.__width = value
+            
+            @property
+            def height(self):
+                """
+                Get the height of the rectangle.
+                """
+                return self.__height
+        
+            @height.setter
+            def height(self, value):
+                """
+                Set the height of the rectangle.
+        
+                :param value: new height of the rectangle
+                """
+                if not isinstance(value, int):
+                    raise TypeError("height must be an integer")
+                if value <= 0:
+                    raise ValueError("height must be > 0")
+                self.__height = value
+            
+            @property
+            def x(self):
+                """
+                Get the x coordinate of the rectangle.
+                """
+                return self.__x
+        
+            @x.setter
+            def x(self, value):
+                """
+                Set the x coordinate of the rectangle.
+        
+                :param value: new x coordinate of the rectangle
+                """
+                if not isinstance(value, int):
+                    raise TypeError("x must be an integer")
+                if value < 0:
+                    raise ValueError("x must be >= 0")
+                self.__x = value
+            
+            @property
+            def y(self):
+                """
+                Get the y coordinate of the rectangle.
+                """
+                return self.__y
+        
+            @y.setter
+            def y(self, value):
+                """
+                Set the y coordinate of the rectangle.
+        
+                :param value: new y coordinate of the rectangle
+                """
+                if not isinstance(value, int):
+                    raise TypeError("y must be an integer")
+                if value < 0:
+                    raise ValueError("y must be >= 0")
+                self.__y = value
