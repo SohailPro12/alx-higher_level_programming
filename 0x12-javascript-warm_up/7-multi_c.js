af6959b0f90cb0d097568641d47a7c5be1ce7aad
Sohail Charef <charefsohayl@gmail.com>
2024-01-08 11:57:11 -0800

    Updated

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const x = process.argv[2];
+if (isNaN(x) || x === undefined) {
+  console.log("Missing number of occurrences");
+} else {
+  const num = parseInt(x);
+  for (let i = 0; i < num; i++) {
+    console.log("C is fun");
+  }
+}
