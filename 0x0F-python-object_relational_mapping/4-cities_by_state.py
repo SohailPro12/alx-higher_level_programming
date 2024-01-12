4fa03673212e738d060fb7239cb9922956c2a2e7
Sohail Charef <charefsohayl@gmail.com>
2024-01-12 09:32:27 -0800

    Updated

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,7 +2,6 @@
 
 """
  lists all states from the database hbtn_0e_0_usa that has the name
- Safe against injection
 """
 
 
@@ -14,14 +13,12 @@ if __name__ == '__main__':
     user_name = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    name = sys.argv[4]
 
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=user_name, passwd=password, db=db_name)
 
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name=%s
-                   ORDER BY states.id ASC; """, (name,))
+    cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC;")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
