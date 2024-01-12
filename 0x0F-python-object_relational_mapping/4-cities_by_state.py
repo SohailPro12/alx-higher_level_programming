c131299f7a36535fe9522831be6b18006d88f440
Sohail Charef <charefsohayl@gmail.com>
2024-01-12 09:48:54 -0800

    Updated

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
                     FROM cities LEFT JOIN states\
-                    ON states.id=cities.id\
+                    ON states.id=cities.state_id\
                     ORDER BY cities.id ASC;")
     rows = cursor.fetchall()
     for row in rows:
