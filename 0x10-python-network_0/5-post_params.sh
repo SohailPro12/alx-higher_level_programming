e1aa39914131c078182e23d551d5692ef2b922b4
Sohail Charef <charefsohayl@gmail.com>
2024-01-25 03:00:53 -0800

    update

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# takes in a URL as an argument, sends a POST request to the URL, and displays the body of the response
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
