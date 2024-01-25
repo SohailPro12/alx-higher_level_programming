dd5d0bed8b13c622d1cc3feceeb1b45862c4b297
Sohail Charef <charefsohayl@gmail.com>
2024-01-25 04:59:18 -0800

    interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,8 +10,9 @@ def find_peak(list_of_integers):
     left = 0
     right = len(list_of_integers)-1
 
-    while left <= right:
+    while left < right:
         mid = (right + left) // 2
+        
         if list_of_integers[mid] > list_of_integers[mid+1]:
             right = mid
         elif:
