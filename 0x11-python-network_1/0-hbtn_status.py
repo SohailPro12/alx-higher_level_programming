c1142fede610920ab55ffaa1cd8601dc1b6a8982
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-01-26 15:24:48 +0100

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,13 @@
 from urllib import request, error
 
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
 
-    with request.urlopen(url) as response:
-        body = response.read()
-        body_decoded = body.decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body_decoded))
+url = "https://alx-intranet.hbtn.io/status"
+
+with request.urlopen(url) as response:
+    body = response.read()
+    body_decoded = body.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body_decoded))
