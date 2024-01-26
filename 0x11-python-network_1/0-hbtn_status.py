73ac47a9dddefba2447846c00ae55ee68c132a01
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-01-26 15:26:20 +0100

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ with request.urlopen(url) as response:
     body = response.read()
     body_decoded = body.decode('utf-8')
     print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
     print("\t- utf8 content: {}".format(body_decoded))
