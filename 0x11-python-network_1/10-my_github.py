101a3c4b215af8c3f225f6343a2c4bcca884c682
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-01-26 17:27:52 +0100

    update

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+
+    try:
+        response = requests.post(url, data=data)
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
