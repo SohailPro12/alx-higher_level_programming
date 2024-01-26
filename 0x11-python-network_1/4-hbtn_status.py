7da0addc864353c6962a103042a89b5c1fc30680
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-01-26 16:17:50 +0100

    update

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status """
+
+import requests
+
+
+url = "https://alx-intranet.hbtn.io/status"
+body = requests.post(url)
+print("Body response:")
+print(f"\t- type: {type(body.content.decode('utf-8'))}")
+print(f"\t- content: {body.content.decode('utf-8')}")
+
