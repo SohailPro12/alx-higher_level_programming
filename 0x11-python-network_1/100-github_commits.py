4503545b2c4a5168a9aa252d4bbcc836fc900c56
Sohail Charef <charefsohayl@gmail.com>
2024-01-27 03:06:43 -0800

    updated

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+list 10 commits (from the most recent to oldest) of a repository
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    rep = sys.argv[1]
+    own = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+                     rep, own)
+
+    r = requests.get(url)
+    j = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                j[i]["sha"],
+                j[i]["commit"]["author"]["name"]))
+    except IndexError:
+        pass
