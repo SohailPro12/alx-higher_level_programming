fd1e6eb3eacf8f69fab8d2c3cf8e6178a312e598
Sohail Charef <charefsohayl@gmail.com>
2024-01-27 02:22:29 -0800

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 url = "https://alx-intranet.hbtn.io/status"
-body = requests.post(url)
+body = requests.get(url)
 print("Body response:")
 print(f"\t- type: {type(body.content.decode('utf-8'))}")
 print(f"\t- content: {body.content.decode('utf-8')}")
