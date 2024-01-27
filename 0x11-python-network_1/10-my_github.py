2856751aa62de411dd7debdd9c3dbda7fbb2e0e2
Sohail Charef <charefsohayl@gmail.com>
2024-01-27 02:35:30 -0800

    updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,23 +6,10 @@ and uses the GitHub API to display your id
 
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        q = ""
-    else:
-        q = sys.argv[1]
-
-    url = "http://0.0.0.0:5000/search_user"
-    data = {"q": q}
-
-    try:
-        response = requests.post(url, data=data)
-        json_data = response.json()
-
-        if json_data:
-            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
