3c8f04bbd592597a4f4c1207786f9159ef003778
Sohail Charef <charefsohayl@gmail.com>
2024-01-27 02:29:24 -0800

    updated

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,9 +10,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    email = {'email': sys.argv[2]}
 
-    data = {'email': email}
-    response = requests.post(url, data)
-    content = response.text
-    print("Response body: {}".format(content))
+    response = requests.post(url, data=email)
+    print(response.text)
