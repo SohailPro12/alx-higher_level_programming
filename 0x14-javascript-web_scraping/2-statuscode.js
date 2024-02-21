d43dc66cc98046d1f88b6440029641b3dc22a1fc
Sohail Charef <charefsohayl@gmail.com>
2024-02-21 04:25:42 -0800

    js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
 const request = require('request');
-const URL = process.argv[2];
-request(URL, function (error, response, body) {
-  console.log('code: ' + response.statusCode);
+const url = process.argv[2];
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
 });
