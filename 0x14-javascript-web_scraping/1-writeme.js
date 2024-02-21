e63928ead0a7e4a18caacf52a726e9ecf8093a47
Sohail Charef <charefsohayl@gmail.com>
2024-02-21 04:10:51 -0800

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const content = process.argv[3]; 
+const content = process.argv[3];
 fs.writeFile(filePath, content, (err) => {
   if (err) throw err;
 });
