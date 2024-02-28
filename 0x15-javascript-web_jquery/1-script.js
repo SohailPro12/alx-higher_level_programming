3c4af46cee2bfd7edbb4cb427f23bd3d65c77137
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-02-28 16:51:44 +0100

    Jquery change color

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -3,6 +3,8 @@ updates the text color of the <header> element to red (#FF0000)
 using JQuery API
 */
 <script src='https://code.jquery.com/jquery-3.2.1.min.js' />;
-const element = $('header');
-element.css('color', 'red');
+  <script>
+    const element = $('header');
+    element.css('color', 'red');
+  </script>
 
