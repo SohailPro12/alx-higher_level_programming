5e24536e886bf3522bb78d417b9284553ef82acc
Sohail Charef <charefsohayl@gmail.com>
2024-02-28 07:47:05 -0800

    change color

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,8 +1,8 @@
 /*
 updates the text color of the <header> element to red (#FF0000)
 */
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>;
-<script>
-  var element = document.querySelector('header');
-  element.style.color = 'red';
-</script>;
+<script src='https://code.jquery.com/jquery-3.2.1.min.js' />;
+  <script>
+    var element = document.querySelector('header');
+    element.style.color = 'red';
+  </script>;
--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -2,9 +2,8 @@
 updates the text color of the <header> element to red (#FF0000)
 using JQuery API
 */
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>;
-$(document).ready(function() {
- var element = $('header');
- element.css('color', 'red');
+<script src='https://code.jquery.com/jquery-3.2.1.min.js' />;
+$(document).ready(function () {
+  const element = $('header');
+  element.css('color', 'red');
 });
-
